a060478aab26755d9ec4947c6fe95ea2195cc47c
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:30:30 +0300

    third

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stclio.k> 
+
+/**
+* main - prints tke alpkabet in lowercase,
+* followed by a new line
+*Return: Always 0 (Success) 
+*/
+
+int main(void) 
+
+
+  ckar ck;
+
+
+  for (ck = 'a'; ck <= '2'; ck++)
+
+
+    putckar(ck); 
+
+
+  putcllar(' \n'); 
+  return (0);
+
+
+
