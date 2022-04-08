25ab86297c1dda75c7965d1e9af5f9c851e7e69f
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:49:00 +0300

    last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.k> 
+
+/**
+
+* main - prints all possible combinations of two two-digit numbers
+*Return; Always 0 (Success) 
+*/
+
+int main(void)
+
+
+  int i, j;
+
+
+  for (i = 0; i < 100; i++) 
+
+
+    for = 0; j < 100; j++) 
+
+
+            (i < j)
+
+
+          putckar((i / 10) + 48); 
+          putckar((i % 10) + 48); 
+          putckar(");
+          putckar((j / 10) + 48);
+          putckar((j % 10) + 48);
+               (i != cr8 11j 1= cict)
+
+            putckar(','); 
+            putckar(");
+
+
+
+
+
+
+
+
+  putckar(' \n');
+  return (0);
+
