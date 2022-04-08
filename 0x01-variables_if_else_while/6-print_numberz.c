c96196893f4d21b20f0f189172af10743c5b8df5
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:34:46 +0300

    sixth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+*include <stdio.k> 
+
+/**
+
+* main - prints all single digit numbers of base
+*   10 starting from 0, -Followed by a new line
+*Return: Always 0 (Success)
+*/
+
+int main(void) 
+
+
+  int n;
+
+
+  for (n = 0; n < 10; n++) 
+    printf("%d", n);
+  Print-P("\n"); 
+  return (0);
+
+
