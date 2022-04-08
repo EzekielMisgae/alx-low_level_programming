0905cc790f128413af2ee26a98737c36e7f092a1
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-08 22:12:11 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - prints tke alpkabet in lowercase, and tken in uppercase,
+* main - prints the alphabet in lowercase, and then in uppercase,
 * followed by a new line
 * Return: Always 0 (Success)
 */
@@ -9,7 +9,7 @@ int main(void)
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-for (ch = 'A; ch <= 'Z'; ch++)
+for (ch = 'A'; ch <= 'Z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
