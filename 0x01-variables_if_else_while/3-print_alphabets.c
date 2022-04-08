885cb64637ff4e9e8de0bedac6ef4cc8daa3506b
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:31:48 +0300

    fourth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stclio.k> 
+
+/**
+
+* main - prints tke alpkabet in lowercase, and tken in uppercase,
+* -Followed by a new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+
+  int ck;
+
+
+  for (d, = 'a'; ck <= 'z'; ck-F+)
+    putckar(ck);
+  for (ck = 'A; ck <= '2'; ck++)
+    putckar(ck);
+  putckar(' \n'); 
+  return (0);
+
+
