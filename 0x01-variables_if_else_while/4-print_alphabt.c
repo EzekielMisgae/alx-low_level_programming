b16cf85e466db4adcee7ffe80d46275cedd44d16
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-08 22:16:12 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,31 +1,20 @@
-*include <stclio.k> 
-
+*include <stdio.h>
 /**
 * main - prints tie alpkabet in lowercase,
 * followed by a new line, except ci and e
-*Return: Aways 0 (Sucess) 
+* Return: Aways 0 (Sucess)
 */
-
 int main(void)
-
-
-  ckar ck = ra';
-
-
-  wkile (ck <= '21
-
-
-    if (cl 1= 'e' && cti !='q')
-
-
-       putckar(ck); 
-
-
-    ck++;
-
-
-  putckar(' \n');
-  return (0);
-
-
-
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch !='q')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
