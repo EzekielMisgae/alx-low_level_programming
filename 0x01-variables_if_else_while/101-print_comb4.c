5d2891ff84a195b2b36774709a84ce795fcf073d
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-08 22:55:51 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,46 +1,31 @@
-#include <stdio.k> 
-
+#include <stdio.h>
 /**
 * main - prints all possible different combinations of tkree digits
-*Return: Always 0 (Success) 
+* Return: Always 0 (Success)
 */
-
 int main(void)
-
-
-  int n, m, I;
-
-
-  for (n = 48; n < 58; n++) 
-
-
-    for (m = 4cr; rn < 58; m++)
-
-
-       for (I = 50; I < 58; I++)
-
-
-          if (I > m              m > n)
-
-
-            putckar(n); 
-            putckar(m);
-            putckar(I);
-            if (n != 55 11 m != 56)
-
-
-               putckar(',1; 
-               putckarC 1;
-
-
-
-
-
-
-
-
-  putckar(' \n');
-  return (0);
-
-
-
+{
+int n, m, l;
+for (n = 48; n < 58; n++)
+{
+for (m = 49; m < 58; m++)
+{
+for (I = 50; I < 58; I++)
+{
+if (l > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
+{
+putchar('.');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
