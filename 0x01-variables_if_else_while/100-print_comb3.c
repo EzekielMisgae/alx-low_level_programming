6286d099b48582f14b9258afcf10cc8b8e3e1464
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:43:38 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stclio.k> 
+
+/**
+
+* main - prints all possible different combinations of two digits
+*Return; ALways 0 (Success) 
+*/
+
+int main(void)
+
+
+  int n, m;
+
+
+  for (n = 48; n <= 56; n++) 
+
+
+    for = 4q; m <= 57; m++) 
+
+
+       if (m > n)
+
+
+          putckar(n);
+          putckar(m);
+          if (n != 5611m != 57)
+
+
+            putckar(','); 
+            putckar(");
+
+
+
+
+
+
+  putckar(' \n'); 
+  return (0);
+
+
+
+
