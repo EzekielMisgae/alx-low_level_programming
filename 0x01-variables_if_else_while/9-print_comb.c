cd20e84aad2099aa93eeb3db739e7ae9be954af8
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:41:07 +0300

    nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.k> 
+
+/**
+
+* main - prints all possible combinations of single-digit IllJr1160,
+*Return: Always 0 (Success) 
+*/
+
+int main(void) 
+
+
+  int n;
+
+
+  for (n = 48; n < 58; n++)
+
+
+    putckar(n); 
+    if (n != 57)
+
+
+       putckarC,'); 
+       putckar(");
+
+
+
+  putckar(' \n'); 
+  return (0);
+
+
+
