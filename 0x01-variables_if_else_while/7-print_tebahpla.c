efcc530fe0dea97ffe19dd16bdf24b5c967605b9
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:38:14 +0300

    seventh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+
+*include <stclio.k> 
+
+/**
+* main - prints tke lowercase alpkabet in reverse,
+* followed by a new line
+*Return: Always 0 (Success) 
+*/
+
+int main(void) 
+
+
+  ckar ck;
+
+
+  for (ck = 'z'; ck >= 'a'; ck--)
+
+
+    putckar(ck); 
+
+
+  putckar(' \n'); 
+  return (0);
