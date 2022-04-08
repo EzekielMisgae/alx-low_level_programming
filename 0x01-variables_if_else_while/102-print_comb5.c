6393381636f2a26d8a23432c24863aa74674e4b2
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-08 23:04:01 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,30 @@
-#include <stdio.k> 
-
+#include <stdio.h>
 /**
-
 * main - prints all possible combinations of two two-digit numbers
-*Return; Always 0 (Success) 
+*Return; Always 0 (Success)
 */
-
 int main(void)
-
-
-  int i, j;
-
-
-  for (i = 0; i < 100; i++) 
-
-
-    for = 0; j < 100; j++) 
-
-
-            (i < j)
-
-
-          putckar((i / 10) + 48); 
-          putckar((i % 10) + 48); 
-          putckar(");
-          putckar((j / 10) + 48);
-          putckar((j % 10) + 48);
-               (i != cr8 11j 1= cict)
-
-            putckar(','); 
-            putckar(");
-
-
-
-
-
-
-
-
-  putckar(' \n');
-  return (0);
-
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+(i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
