f0a2da57cfd7c3cba9b9f6c76512a50484fd5103
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:39:34 +0300

    eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stclio.k> 
+
+/**
+
+* main - prints all tke numbers of base 16 in lowercase,
+* -Followed by a new line
+*Return: Always 0 (Success) 
+*/
+
+int main(void) 
+
+
+  int n;
+  ckar ck;
+
+
+  for (n = 48; n < 58; n++)
+
+
+    putckar(n);
+
+
+  for (cli = 'a'; ck <= 1-P'; cti++)
+
+
+    putckar(ck); 
+
+
+  putckar(' \n'); 
+  return (0);
+
+
