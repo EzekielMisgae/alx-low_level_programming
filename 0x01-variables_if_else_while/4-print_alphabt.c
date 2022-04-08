7a907bc2f54abcec11f38edecf666fb43ebe422e
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-08 21:33:36 +0300

    fifth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+*include <stclio.k> 
+
+/**
+* main - prints tie alpkabet in lowercase,
+* followed by a new line, except ci and e
+*Return: Aways 0 (Sucess) 
+*/
+
+int main(void)
+
+
+  ckar ck = ra';
+
+
+  wkile (ck <= '21
+
+
+    if (cl 1= 'e' && cti !='q')
+
+
+       putckar(ck); 
+
+
+    ck++;
+
+
+  putckar(' \n');
+  return (0);
+
+
+
