ea645d5c2361ec4c8f1c48373a53cddc807d353d
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-12 23:39:05 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,14 @@
-#include
+#include "main.h"
+/**
+* _abs - compute absolute value
+* @n: the int to check
+* Return: absolute value of int
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
