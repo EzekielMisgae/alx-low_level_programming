1db9349a977f90b5eca03dd0a8b44fcf765dad98
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-14 22:03:24 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * print_most_numbers - print the numbers from 0 to 9
 * except 2 and 4, followed by a new line
@@ -14,5 +14,4 @@ if (i != 2 && i != 4)
 _putchar(i + '0');
 }
 _putchar('\n');
-}
 }
