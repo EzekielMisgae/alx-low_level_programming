b5dbe6853706f830e4be6f612f8474b35564ac7a
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-14 23:21:53 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "math.h"
+#include <math.h>
 /**
-* main - finds and prints the largest prime factor of the number 1
+* main - finds and prints the largest prime factor of the number 612852475143
 * followed by a new line
 * Return: Always 0 (Success)
 */
