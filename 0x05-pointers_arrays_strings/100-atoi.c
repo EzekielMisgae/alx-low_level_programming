8e3be8992695b7f46f5c61942d9914907571a2cf
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-18 22:28:27 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,35 +6,38 @@
 */
 int _atoi(char *s)
 {
-int i, j, k, l, m, n;
+int i, d, n, len, f, digit;
 
 i = 0;
-j = 0;
-k = 0;
-l = 0;
-m = 0;
+d = 0;
 n = 0;
+len = 0;
+f = 0;
+digit = 0;
 
-while (s[l] != '\n')
-l++;
-while (i < l && m == 0)
+while (s[len] != '\0')
+len++;
+
+while (i < len && f == 0)
 {
+
 if (s[i] == '-')
-++j;
+++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
-n = s[i] - '0';
-if (j % 2)
-n = -n;
-k = k * 10 + n;
-m = 1;
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
-m = 0;
+f = 0;
 }
 i++;
 }
-if (m == 0)
+if (f == 0)
 return (0);
 return (n);
 }
