0edd7859a5286914c9c2384c6dbb134ba0b9d17d
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-18 15:11:25 +0300

    2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#iclude "main.h"
+
+/**
+* swap_int - swap value of integers 
+* @a: pointer to first
+* @b: pointer to second
+*/
+
+void swap_int(int *a, int *b);
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+
+}
