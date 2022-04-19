732bdacd41275ce4d1b33aec6f477fe5981dc297
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-19 19:10:53 +0300

    six put

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts2 - print character of string followed by a new line
+* @str - string to print the character from
+*/
+
+void puts2(char *str)
+{
+int len, i;
+len = 0;
+
+while 
+
+
+
+
+
+
+
+
+
+
+
+}
