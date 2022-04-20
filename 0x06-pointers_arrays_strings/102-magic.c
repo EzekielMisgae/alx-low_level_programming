1ea2ecd01d4125d87887388b65806b7b3051e657
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-20 21:42:55 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,11 +9,14 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* You are not allowed to use the variable a in your new line of code
-* You are not allowed to modify the variable p
-* You can only write one statement
-* You are not allowed to use ,
-* (p + 5) = 98;
+* write your line of code here
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line pf code
+*/
+*(p + 5) = 98;
 /* ...so this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
