64261b60918af86df0a9ecc745aba0dffea2a27a
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-20 20:36:00 +0300

    Update _putchar.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -3,5 +3,11 @@
 * _putchar - writes the character c to stdout
 * @c: the character to print
 * Return: On success 1
-* On error 0
+* On error, -1 is returned
 */
+int _putchar(char c)
+{
+
+return (write(1, &c, 1));
+
+}
