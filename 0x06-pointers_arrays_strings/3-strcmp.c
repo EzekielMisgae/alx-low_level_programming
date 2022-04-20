025d8b93930ed4af137e0e80e50355b0f9c5b9fd
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-20 20:49:31 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,10 @@
 #include "main.h"
-
-
+/**
+* _strcmp - compare two strings
+* @s1: first string to compare
+* @s2: second string to compare
+* Return: < 0 if s1 < s2, if s1 = s2 0, > 0 if s1 > s2 
+*/
 
 int _strcmp(char *s1, char *s2)
 {
