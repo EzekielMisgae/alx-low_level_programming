edb45127ee8464f706e8365419e079be79c763b9
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-20 21:05:29 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+/**
+* *string_toupper - changes all lowercase to upper case
+* @s:string to modify
+* Return: the string result
+*/
 
-
-
-
-
+char *string_toupper(char *)
 {
 int i;
 for (i = 0; s[i] != '\0'; i++)
