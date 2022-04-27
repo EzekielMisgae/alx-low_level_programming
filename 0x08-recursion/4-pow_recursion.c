feb6b90d1c047c2398ccd63db117d6329df69cb2
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-04-27 15:54:54 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,15 +3,14 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: number to be used
- * @y: exponent to be used
- *
+ * @y: exponent number
  * Return: x ^ y
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
 }
