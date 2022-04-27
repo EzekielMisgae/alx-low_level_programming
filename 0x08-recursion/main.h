cc328a22301cd64ebbbee4ff3cb1546bb6d9b6b5
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-27 13:04:10 +0300

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#endif _MAIN_H
-#indef _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,4 +11,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endef
+#endif
