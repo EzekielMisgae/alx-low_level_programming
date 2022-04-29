a9a94700051de6ee00f9a594693a9918ced1be1d
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:54:51 +0300

    commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,13 +1,11 @@
-
-   
 #include "main.h"
 
 /**
-*main - prints the minimum number of coins to make change
+* main - prints the minimum number of coins to make change
 * for an amount of money
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: returns 1 if there is an error; else returns 0
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: returns 1 if there is an error; else returns 0
 */
 
 int main(int argc, char *argv[])
