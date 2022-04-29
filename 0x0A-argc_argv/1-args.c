f6d0d53e95ab18709f249465de7a12d205241431
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:35:28 +0300

    commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
-* main: Prints the number of arguments
-* @argc: the number of arguments
-* @argv: An array of argument
-* Return: number of argument
+* main - prints the number of arguments
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: the number of arguments
 */
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
+
 return (0);
 }
