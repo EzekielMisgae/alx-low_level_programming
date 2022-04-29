ac1b3527440d7d5ed1ab71a4870428bccb1ed8ec
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:15:08 +0300

    commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,2 +1,15 @@
 #include "main.h"
-#inlude <stdio.h>
+
+/**
+* main: Display the number
+* @argc: The number of arg supplied to the program
+* @argv: An array of pointers to the argument
+* @n: argument value of except 0
+* Return: Always number
+*/
+
+int main(int argc, char* argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
