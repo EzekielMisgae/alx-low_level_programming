453b7acb1af563db6768bf0926bbd002e4864a85
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:01:42 +0300

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+* main - Display the program name, followed by a new line.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
