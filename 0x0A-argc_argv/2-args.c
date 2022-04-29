013a3402e58c464cdb5ae583f13722aae4c346f9
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:39:26 +0300

    commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main: list arguments
-* @argc: number of arguments
-* @argv: arguments passed
-* Return: 0 (successful)
+* main: listing the arguments
+* @argc: number of arguments passed on excuted
+* @argv: arguments passed when excuted
+* Return: 0 on successfull completion
 */
 
 int main(int argc, char *argv[])
 {
 int i = 0;
-for (i < argc; i++)
+for (; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
