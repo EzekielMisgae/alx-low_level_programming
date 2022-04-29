48976bff4ff04b36861d6ac5e095fc0e783971bc
EzekielMisgae <ezkielmisgae@gmail.com>
2022-04-29 14:49:14 +0300

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,26 @@
 include "main.h"
 
 /**
- * main - Multiplyes two numbers
- * @argc: numbers of argument passed
- * @argv: arguments
- * Return: 1 if its has falure otherwise 0
+* main - Multiplyes two numbers
+* @argc: numbers of argument passed
+* @argv: arguments
+* Return: 1 if its has falure otherwise 0
 */
+
 int main(int argc, char *argv[])
 {
-	int i, j, mul;
+int i, j, mul;
 
-	if (argc <= 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	mul = i * j;
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+mul = i * j;
 
-	printf("%d\n", mul);
-	return (0);
+printf("%d\n", mul);
+return (0);
 }
