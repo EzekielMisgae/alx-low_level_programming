3e617cd8142a5ce422a5220bea1bc55d44cd02cb
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-05-02 10:38:15 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
@@ -8,22 +7,18 @@
 int _atoi(char *s)
 {
 int i, d, n, len, f, digit;
-
 i = 0;
 d = 0;
 n = 0;
 len = 0;
 f = 0;
 digit = 0;
-
 while (s[len] != '\0')
 len++;
-
 while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
-
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
@@ -37,7 +32,6 @@ f = 0;
 }
 i++;
 }
-
 if (f == 0)
 return (0);
 return (n);
