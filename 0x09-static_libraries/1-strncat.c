47cca11e8875cf8114e43a008d39672e49f0f85d
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-05-02 10:37:36 +0300

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _strncat - Concatenates two strings using at most
 *            an inputted number of bytes from src.
@@ -11,7 +10,6 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int index = 0, dest_len = 0;
-
 while (dest[index++])
 dest_len++;
 for (index = 0; src[index] && index < n; index++)
