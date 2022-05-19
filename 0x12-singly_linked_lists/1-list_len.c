64a2cd3e02030735dd9e8df018c17f7e60d4fd60
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-19 14:54:24 +0300

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+* list_len - number of elements list
+* @h: pointer to structure
+* Return: length.
+*/
+
+size_t list_len(const list_t *h)
+
+{
+unsigned int i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
+
