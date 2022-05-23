f7eafe9b63ccb4a20e42b6d483b54ae5dc86b486
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-23 20:09:59 +0300

    commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,21 @@
-#ifndef _list_h
-#define _list_h
+#ifndef _lists_h
+#define _lists_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*/
+
+typedef struct listint_s
+
+{
+int n;
+struct listint_s *next;
+}
 
 listint_t;
 size_t print_listint(const listint_t *h);
