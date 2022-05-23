d6727b37d7776ad2eab25a796cc4fa7ea73bd946
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-23 20:06:33 +0300

    commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _lists_h
+#define _lists_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -13,8 +17,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
 #endif
