9da4710d64a817332c18697cb170cc971f38bb73
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-24 09:41:44 +0300

    commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+/**
+* pop_listint - return data from and then delete memory allocated
+* @head: double pointer to the first member of a singly linked list
+* Return: int value of node deleted from start of list
+*/
+
+int pop_listint(listint_t **head)
+
+{
+listint_t *old_h;
+int num;
+if (!head)
+return (0);
+if (*head)
+{
+old_h = *head;
+*head = (*head)->next;
+num = old_h->n;
+free(old_h);
+return (num);
+}
+else
+return (0);
+}
