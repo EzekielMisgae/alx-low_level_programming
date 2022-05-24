3c2de02f81e29c2f92d8d39aa7e8115659f2b764
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-24 10:39:23 +0300

    commit

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,2 +1,26 @@
 #include "lists.h"
 
+/**
+* sum_listint - returns sum of all data of linked list
+* @head: pointer to the first member of a singly linked list of structs
+* Return: sum of all the 'int n' elements of each struct in linked list
+*/
+
+int sum_listint(listint_t *head)
+
+{
+
+litint_t *temp = head;
+int sum = 0;
+if (!head)
+return (0);
+while (temp->next)
+{
+sum += temp->n;
+temp = temp->next;
+}
+if (!temp->next)
+sum += temp->n;
+return (sum);
+
+}
