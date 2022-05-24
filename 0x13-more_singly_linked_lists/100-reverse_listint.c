a4b4d31ea3ae261e6ebb44d55b4eb3de127167ea
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-24 10:57:55 +0300

    commit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,1 +1,28 @@
+
+   
 #include "lists.h"
+
+/**
+ * reverse_listint - reverse a linked list
+ * @head: pointer to head
+ * Return: pointer to head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *current, *next;
+
+	if (head == NULL)
+		return (0);
+	prev = NULL;
+	current = *head;
+	next = NULL;
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (*head);
+}
