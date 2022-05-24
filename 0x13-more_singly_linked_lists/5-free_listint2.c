e131d9e1d7897e454bbca4bc03f1fe2e625908e0
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-24 09:35:06 +0300

    commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,31 @@
-#include "main.h"
+#include "lists.h"
+
+/**
+* free_listint2 - free memory previously allocated to a struct list
+* @head: double pointer to the first member of a singly linked list
+*/
+
+void free_listint2(listint_t **head)
+
+{
+
+listint_t *temp;
+
+if (head == NULL)
+return;
+
+while (*head)
+{
+if ((*head)->next)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+else
+{
+free(*head);
+*head = NULL;
+}
+}
+}
