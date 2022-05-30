acde63df12baf51166c812c9789aa2911fc1af5e
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-30 22:22:07 +0300

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,11 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <elf.h>
 
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+elf_header elf_filename
 
 #endif
